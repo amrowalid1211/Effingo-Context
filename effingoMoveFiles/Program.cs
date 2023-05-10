0c0caadd0eeb7ec3672442aeffa5989cc69f76b9
siriusamro <73645885+siriusamro@users.noreply.github.com>
2023-05-10 04:11:50 +0300

    Add project for moving files to target default destination

---
+++ effingoMoveFiles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+static void Main(string[] main)
+{
+    Console.WriteLine("SA");
+    moveFiles(main);
+
+}
+Main(args);
+
+static void moveFiles(string[] main)
+{
+    try
+    {
+        string targetFolder = File.ReadAllText("defaultPath.txt");
+        for (int i = 0; i < main.Length; i++)
+        {
+            //MessageBox.Show(i + " " + Path.GetExtension(main[i]));
+            if(Path.GetExtension(main[i])==".pdf")
+                File.Copy(main[i], Path.Combine(targetFolder, Path.GetFileName(main[i])));
+        }
+        //MessageBox.Show(File.ReadAllText("defaultPath.txt"));
+    }
+    catch (Exception ex)
+    {
+        //MessageBox.Show(File.ReadAllText(ex.Message));
+    }
+}
